6fb608b9d6d66f08cba8b0df7f024ad7110d694c
Guillermo Fernández <guillermofr@digio.es>
2015-10-16 19:27:20 +0200

    landing molon

---
+++ application/views/welcome_message2.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+		<title>INTERESTELEGRAM</title>
+		<meta name="description" content="App Showcase with Grid Overlay" />
+		<meta name="keywords" content="app, showcase, css transitions, perspective, 3d, grid, overlay, css3, jquery" />
+		<meta name="author" content="Codrops" />
+		<link rel="shortcut icon" href="../favicon.ico"> 
+		<link rel="stylesheet" type="text/css" href="css/default.css" />
+		<link rel="stylesheet" type="text/css" href="css/landing.css" />
+		<script src="js/modernizr.custom.js"></script>
+	</head>
+	<body>
+		<div class="container">	
+			<!-- Codrops top bar -->
+			<div class="codrops-top clearfix">
+				
+			</div><!--/ Codrops top bar -->
+			<header class="clearfix">
+				<div class="inner">
+					<!--h1><span></span></h1-->
+					<nav class="codrops-demos">
+						<a class="current-demo" href="#">Inicio</a>
+						<a href="#">Ayuda</a>
+						<a href="#">Ranking</a>
+						<a href="#">Contacto</a>
+					</nav>
+				</div>
+			</header>
+			<div class="main">
+				<div id="ac-wrapper" class="ac-wrapper">
+					<h2>Interestelegram<span>Juega con tus amigos y conquista la galaxia.</span></h2>
+					<div class="ac-device">
+						<a href="#"><img src="images/screen1.png"/></a>
+						<h3 class="ac-title">Gratis a través de Telegram</h3>
+						<nav>
+							<span>&lt;</span>
+							<span>&gt;</span>
+						</nav>
+					</div>
+					<div class="ac-grid">
+						<a href="#"><img src="images/screen1.png"/><span>Gratis a través de Telegram</span></a>
+						<a href="#"><img src="images/screen2.png"/><span>Explora el universo.</span></a>
+						<a href="#"><img src="images/screen3.png"/><span>Ataca a otros jugadores.</span></a>
+						<a href="#"><img src="images/screen4.png"/><span>Persigue a otras naves.</span></a>
+						<a href="#"><img src="images/screen5.png"/><span>Compite en el ranking.</span></a>
+						<a href="#"><img src="images/screen6.png"/><span>Consigue recursos de tu entorno.</span></a>
+						<a href="#"><img src="images/screen7.png"/><span>Mejora tu nave y arrasa.</span></a>
+						<a href="#"><img src="images/screen8.png"/><span>Coopera con amigos.</span></a>
+					</div>
+				</div>	
+				<p>Las imágenes ofrecidas no se corresponden con la realidad.</p>			
+			</div>
+		</div><!-- /container -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script src="js/landing.js"></script>
+		<script>
+			$(function() {
+				AppShowcase.init();
+			});
+		</script>
+	</body>
+</html>
+
